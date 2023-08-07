2d9a512a97d43ac82cca70834a22b8a62eee20ee
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2023-08-07 21:34:16 +0530

    workingtrain

--- train/numbersapi/src/App.js
+++ train/numbersapi/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [urls, setUrls] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get(urls);
+      setResult(response.data.numbers.join(', '));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setResult('Error fetching data from server.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Number Management App</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter URLs (separated by "&url="):
+          <input
+            type="text"
+            value={urls}
+            onChange={(e) => setUrls(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {result && (
+        <div>
+          <h2>Merged Unique Integers:</h2>
+          <p>{result}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default App;
